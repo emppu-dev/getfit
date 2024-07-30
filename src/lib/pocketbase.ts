b837ea1d1f983f80b65e98c32ab88c3bb3c7145e
emppu-dev <83163481+emppu-dev@users.noreply.github.com>
2024-07-30 18:11:21 +0300

    feat: env configuration

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -7,8 +7,9 @@ https://raw.githubusercontent.com/emppu-dev/getfit/main/LICENSE
 
 import PocketBase from 'pocketbase';
 import { writable } from 'svelte/store';
+import { PUBLIC_API_URL } from '$env/static/public';
 
-export const pb = new PocketBase('https://api.bunk.lol');
+export const pb = new PocketBase(PUBLIC_API_URL);
 
 export const currentUser = writable(pb.authStore.model);
 
